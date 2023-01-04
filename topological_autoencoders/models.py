899ccd75ce09e4fc058cf8c873a24f765392dfba
Marcus  Blake <blakemarcus15@gmail.com>
2023-01-04 06:47:38 -0500

    Update CNN architecture and documentation

--- topological_autoencoders/models.py
+++ topological_autoencoders/models.py
@@ -68,24 +68,24 @@ class ConvolutionalAutoencoder(AutoencoderBase):
     def __init__(self, latent_space_dim: int = 2):
         super().__init__(latent_space_dim)
         self.encoder =nn.Sequential(
-            nn.Conv2d(1, 96, kernel_size=3, stride=1, padding=1), # Maintains input dimensions 28x28. Include padding at input layer.
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1), # Maintains input dimensions 28x28. Include padding at input layer.
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2), # Downsamples to 14x14
-            nn.Conv2d(96, 32, kernel_size=3, stride=1, padding=1), # Maintins input dimension 14x14
+            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1), # Maintins input dimension 14x14
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2), # Downsamples to 7x7
             nn.Conv2d(32, 8, kernel_size=3, stride=1, padding=1), # Maintains dimension 
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2), # Downsamples to 3x3
-            nn.Flatten(), # Flappens to vector of 3x3x8 since the number of filters from last conv2d layer is 8
+            nn.Flatten(), # Flattens to vector of 3x3x8 since the number of filters from last conv2d layer is 8
             nn.Linear(8*3*3, latent_space_dim)
         )
         self.decoder = nn.Sequential(
             nn.Linear(latent_space_dim, 8*3*3),
             View((-1, 8, 3, 3)), # Reshapes to have 8 channels, and 3x3 arrays.
             nn.ConvTranspose2d(8, 32, kernel_size=3, stride=2), # Upsamples to 7x7
-            nn.ConvTranspose2d(32, 96, kernel_size=2, stride=2), # Upsamples to 14x14
-            nn.ConvTranspose2d(96, 1, kernel_size=2, stride=2) # Upsamples to 28x28
+            nn.ConvTranspose2d(32, 64, kernel_size=2, stride=2), # Upsamples to 14x14
+            nn.ConvTranspose2d(64, 1, kernel_size=2, stride=2) # Upsamples to 28x28
         )
         self.latent_dim = latent_space_dim
 
@@ -99,6 +99,7 @@ class ConvolutionalAutoencoder(AutoencoderBase):
         z = self.encode(x)
         return ForwardOutput(self.decode(z), z)
 
+"""Implementation of variational autoencoder as described in https://arxiv.org/pdf/1312.6114.pdf"""
 class VariationalAutoencoder(AutoencoderBase):
     def __init__(self, latent_space_dim: int = 2):
         super().__init__(latent_space_dim)
